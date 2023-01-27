421d3460951742d5a73d13bc3cbdfad21a20109f
Lucas Costa <thelucascostadasilva@gmail.com>
2023-01-26 23:50:06 -0300

    add new modal to my certifications in the journey section

--- script.js
+++ script.js
@@ -15,6 +15,35 @@ const menu_items = document.querySelectorAll('nav .mainMenu li a');
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
+// Modal Popup certificates
+const btns = document.querySelectorAll("[data-target]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay2 = document.getElementById("overlay2");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector(btn.dataset.target).classList.add("active");
+        overlay2.classList.add("active");
+    });
+});
+
+close_modals.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const modal = btn.closest(".modal");
+        modal.classList.remove("active");
+        overlay2.classList.remove("active");
+    });
+});
+
+window.onclick = (event) => {
+    if (event.target == overlay2) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal) => modal.classList.remove("active"));
+        overlay2.classList.remove("active");
+    }
+};
+
+
 // close menu when you click on a menu item 
 menu_items.forEach(item => {
     item.addEventListener('click', function () {
